1c541133e7eb6df5e20a96f4a7a4534e91a24fd9
dulnith-liyanage <dnethmiraliyanage@gmail.com>
2025-02-06 19:37:20 +0530

    Completed the credit program

--- C/Week1/credit.c
+++ C/Week1/credit.c
@@ -1,14 +1,94 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int check_sum(long pin);
 int get_pin_length(long pin);
+bool check_sum(long pin);
+
+int get_first_digit(long pin);
+int get_second_digit(long pin);
+int get_first_two_digits(int first_digit, int second_digit);
+string get_card_type(long pin);
 
 int main(void)
 {
     long pin = get_long("Number: ");
+    string card_type = get_card_type(pin);
+    printf("%s\n", card_type);
+}
 
-    check_sum(pin);
+string get_card_type(long pin)
+{
+    int pin_length = get_pin_length(pin);
+    int first_digit = get_first_digit(pin);
+    int second_digit = get_second_digit(pin);
+    int first_two_digits = get_first_two_digits(first_digit, second_digit);
+
+    if (check_sum(pin))
+    {
+
+        if (pin_length == 15 && (first_two_digits == 34 || first_two_digits == 37))
+        {
+            return "AMEX";
+        }
+        else if (pin_length == 13 && first_digit == 4)
+        {
+            return "VISA";
+        }
+        else if (pin_length == 16)
+        {
+            if (first_two_digits == 51 || first_two_digits == 52 || first_two_digits == 53 || first_two_digits == 54 || first_two_digits == 55)
+            {
+                return "MASTERCARD";
+            }
+            else if (first_digit == 4)
+            {
+                return "VISA";
+            }
+            else
+            {
+                return "INVALID";
+            }
+        }
+        else
+        {
+            return "INVALID";
+        }
+    }
+    else
+    {
+        return "INVALID";
+    }
+}
+
+int get_first_two_digits(int first_digit, int second_digit)
+{
+    int first_two_digits = first_digit * 10 + second_digit;
+    return first_two_digits;
+}
+
+int get_first_digit(long pin)
+{
+    int pin_length = get_pin_length(pin);
+    int first_digit;
+    for (int i = 0; i < pin_length; i++)
+    {
+        first_digit = pin % 10; // Used to obtain the last digit in the pin
+        pin /= 10;              // Removes the last digit
+    }
+    return first_digit;
+}
+
+int get_second_digit(long pin)
+{
+    int pin_length = get_pin_length(pin);
+    int second_digit;
+
+    for (int i = 0; i < pin_length - 1; i++)
+    {
+        second_digit = pin % 10; // Used to obtain the last digit in the pin
+        pin /= 10;               // Removes the last digit
+    }
+    return second_digit;
 }
 
 int get_pin_length(long pin)
@@ -16,13 +96,13 @@ int get_pin_length(long pin)
     int count = 0;
     while (pin != 0)
     {
-        pin /= 10;
-        count++;
+        pin /= 10; // Removes the last digit in the pin
+        count++;   // Used to determine the length of the pin
     }
     return count;
 }
 
-int check_sum(long pin)
+bool check_sum(long pin)
 {
     int last_digit;
     int count = 0;
@@ -31,9 +111,10 @@ int check_sum(long pin)
 
     while (pin != 0)
     {
-        last_digit = pin % 10;
-        pin /= 10;
+        last_digit = pin % 10; // Used to obtain the last digit in the pin
+        pin /= 10;             // Removes the last digit
 
+        // Used to differentiate odd and even digits starting from the second to last digit
         if (count % 2 == 1)
         {
             int last_digit_doubled = last_digit * 2;
@@ -48,16 +129,15 @@ int check_sum(long pin)
             sum2 += last_digit;
         }
 
-        count++;
+        count++; // Used to move thorugh each single digit
     }
 
-    printf("sum1: %i, sum2: %i\n", sum1, sum2);
     if ((sum1 + sum2) % 10 == 0)
     {
-        printf("VALID\n");
+        return true;
     }
     else
     {
-        printf("INVALID\n");
+        return false;
     }
 }
