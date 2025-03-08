0df6e14a2136372caf17293aeff5bc76de2ee1e9
dulnith-liyanage <dnethmiraliyanage@gmail.com>
2025-03-08 21:11:08 +0530

    Coded the grayscale function

---
+++ C/Week4/filter-more/helpers.c
@@ -0,0 +1,38 @@
+#include "helpers.h"
+#include "math.h"
+#include "stdio.h"
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int average_value = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3);
+
+            image[i][j].rgbtBlue = average_value;
+            image[i][j].rgbtGreen = average_value;
+            image[i][j].rgbtRed = average_value;
+        }
+    }
+    return;
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Detect edges
+void edges(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
